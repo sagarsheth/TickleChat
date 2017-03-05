593a16898304a27a69906567e95b1850f0e75ff8
aparnaDas93 <aparnadas0293@gmail.com>
2017-03-05 22:58:29 +0530

    removed dialog for login and created a new activity(activity_login_new) for the same

--- app/src/main/java/com/techpro/chat/ticklechat/login/SplashActivity.java
+++ app/src/main/java/com/techpro/chat/ticklechat/login/SplashActivity.java
@@ -48,7 +48,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AppEventsLogger.activateApp(this);
+        AppEventsLogger.activateApp(getApplication());
         // Making notification bar transparent
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
